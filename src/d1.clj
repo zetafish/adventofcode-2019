bfa41835da180b02ea9ecf6a7e593129877c996e
Endymion Kasanardjo <zetafish@gmail.com>
2019-12-01 09:52:30 +0100

    Day 1

---
+++ src/d1.clj
@@ -0,0 +1,28 @@
+(ns d1
+  (:require [clojure.java.io :as io]))
+
+(def input
+  (let [lines (with-open [r (io/reader (io/resource "d1.txt"))]
+                (doall (line-seq r)))]
+    (map #(Integer/parseInt %) lines)))
+
+(defn fuel1
+  [mass]
+  (- (quot mass 3) 2))
+
+(defn fuel2
+  [mass]
+  (loop [total 0 mass mass]
+    (let [x (fuel1 mass)]
+      (if (neg? x)
+        total
+        (recur (+ total x) x)))))
+
+(defn solve [f]
+  (fn [input]
+    (->> input
+         (map f)
+         (reduce +))))
+
+((solve fuel1) input)
+((solve fuel2) input)
